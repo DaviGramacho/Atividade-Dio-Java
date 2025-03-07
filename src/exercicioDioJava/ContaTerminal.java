b900db628328f0f44ccc79bd1714bb1e0514dc08
Davi Pires <piresdavi135@gmail.com>
2025-03-06 21:24:06 -0300

    adicionando as ultimas funcionalidades do exercicio

--- src/exercicioDioJava/ContaTerminal.java
+++ src/exercicioDioJava/ContaTerminal.java
@@ -5,10 +5,22 @@ public class ContaTerminal {
 	public static void main(String[]args) {
 		Scanner sc = new Scanner(System.in);
 		
+		String nomeUsuario, numeroAgencia;
 		int numeroConta;
 		double saldoConta;
-		String nomeUsuario, agencia;
+		
+		System.out.println("Por favor, digite o seu nome !");
+		nomeUsuario = sc.next();
 		
 		System.out.println("Por favor, digite o número da Agência !");
+		numeroAgencia = sc.next();
+		
+		System.out.println("Por favor, digite o número da Conta !");
+		numeroConta = sc.nextInt();
+		
+		System.out.println("Por favor, digite o seu saldo em conta !");
+		saldoConta = sc.nextDouble();
+		
+		System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", nomeUsuario, numeroAgencia, numeroConta, saldoConta );
 	}
 }
