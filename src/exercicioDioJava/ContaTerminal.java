70c081c5020b0566d5e634d948b78b736fce1b66
Davi Pires <piresdavi135@gmail.com>
2025-03-06 21:07:01 -0300

    Adicionando o scanner

---
+++ src/exercicioDioJava/ContaTerminal.java
@@ -0,0 +1,14 @@
+package exercicioDioJava;
+import java.util.Scanner;
+
+public class ContaTerminal {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int numeroConta;
+		double saldoConta;
+		String nomeUsuario, agencia;
+		
+		System.out.println("Por favor, digite o número da Agência !");
+	}
+}
